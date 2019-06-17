21e941723e7a35f8eb89c5801c063358d29d746c
Deirdre Connolly <deirdre@zfnd.org>
2019-06-17 02:12:38 -0400

    Update dependency graph generator to cargo-deps

--- tools/draw_graph.sh
+++ tools/draw_graph.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# Cargo graph does not work with cargo workspaces #33
-# https://github.com/kbknapp/cargo-graph/issues/33
-# so first we need to patch Cargo.toml and remove workspace
-patch -R Cargo.toml tools/workspace.diff
+cargo install cargo-deps
 
-# Now let's rebuild Cargo.lock by telling cargo to update local package
-cargo update -p pbtc
-
-# And draw dependencies graph using cargo graph
-cargo graph --build-shape box --build-line-style dashed > tools/graph.dot
+# And draw dependencies graph using cargo deps
+cargo deps > tools/graph.dot
 
 # Let's fix graph ratio
 patch tools/graph.dot tools/graph_ratio.diff
 
+# Requires graphviz. If on macOS: 'brew install graphviz'
 dot -Tsvg > tools/graph.svg tools/graph.dot
-
-# Finally let's bring back old Cargo.toml file
-patch Cargo.toml tools/workspace.diff
-
-# Now let's revert Cargo.lock to previous state
-cargo update -p pbtc
