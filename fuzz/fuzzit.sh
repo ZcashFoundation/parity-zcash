7ebd3353b7e67c91dcef45b74b44cdb9cf89cff3
Deirdre Connolly <deirdre@zfnd.org>
2019-08-21 20:46:01 -0400

    Clean up, add honggfuzz and afl

--- fuzz/fuzzit.sh
+++ fuzz/fuzzit.sh
@@ -10,14 +10,20 @@ if [ -z ${1+x} ]; then
     exit 1
 fi
 
-# Install dependencies
-# cargo-fuzz installed by workflow
+# Install Rust dependencies
+rustup install nightly
+carog install --force cargo-fuzz afl honggfuzz
+
+#Install Fuzzit dependencies
 wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.29/fuzzit_Linux_x86_64
 chmod a+x fuzzit
 
 # Build fuzzers
 cargo +nightly fuzz run crypto -- -runs=0
+cargo afl build
+cargo hfuzz build
 
+# Create Fuzzit job
 if [ $1 == "fuzzing" ]; then
     ./fuzzit auth ${FUZZIT_API_KEY}
     ./fuzzit create job --branch $(git branch --show-current) --revision $GITHUB_SHA zebra/crypto ./fuzz/target/x86_64-unknown-linux-gnu/debug/crypto
